dda3e6c8af31eb626217570df8e976d25db75bdd
Selim Çoban <selim@Selim-MacBook-Pro.local>
2025-06-30 19:28:34 +0300

    fix ;

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,18 @@
-import { useAuthStore } from '../store/useAuthStore';
-import { Link } from 'react-router-dom';
-import { MessageSquare, User, LogOut, Settings } from 'lucide-react';
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
-const Navbar =() =>{
-    const {logout, authUser, } = useAuthStore();
+const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
 
-    return (
-        <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
-
-         <div className="container mx-auto px-4 h-16">
+  return (
+    <header
+      className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
+    backdrop-blur-lg bg-base-100/80"
+    >
+      <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
-        <div className="flex items-center gap-8">
+          <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
@@ -20,7 +22,7 @@ const Navbar =() =>{
           </div>
 
           <div className="flex items-center gap-2">
-          <Link
+            <Link
               to={"/settings"}
               className={`
               btn btn-sm gap-2 transition-colors
@@ -44,14 +46,10 @@ const Navbar =() =>{
                 </button>
               </>
             )}
-            </div>
-
-
+          </div>
         </div>
-        </div>;
-
-        </header>
-    );
-    
+      </div>
+    </header>
+  );
 };
 export default Navbar;
